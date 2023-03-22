d1f2c0e1f155c1866a9ffbe073e82c035c56c3a2
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-03-22 16:31:16 +0900

    Lab2-1

--- Week2/Lab2/Lab2-1.py
+++ Week2/Lab2/Lab2-1.py
@@ -1,3 +1,39 @@
 import cv2 as cv
 import sys
 from matplotlib import pyplot as plt
+
+img = cv.imread('../Lab2 Image/lena_color.png')
+
+if img is None:
+    sys.exit("파일을 찾을 수 없습니다")
+
+print("Image Size = ", img.shape) # image size [height width channel]
+h, w, c = img.shape
+
+print('Pixel Intensity Value = ', img[100, 70]) # access the pixel intensity value at the position of (100, 70)
+
+"""
+OpenCv --> BGR image
+"""
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+roi = img[50:300, 30:280] # image cropping (ROI extraction by slicing)
+
+cv.imshow('Color Image', img)
+cv.imshow('Gray Image', gray)
+cv.imshow('Cropped Image', roi)
+
+"""
+matplotlib --> RGB image
+"""
+
+RGB_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+RGB_roi = RGB_img[50:300, 30:280]
+roi = img[50:300, 30:280]
+plt.imshow(RGB_roi)
+plt.show()
+
+cv.waitKey()
+cv.destroyAllWindows()
+
