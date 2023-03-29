b184e081ef981da2b3e37161752780a638abf647
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-03-29 15:30:04 +0900

    Lab2-2

--- Week2/Lab2/Lab2-2.py
+++ Week2/Lab2/Lab2-2.py
@@ -30,22 +30,22 @@ cv.imshow('Averaging Image Gray', dst_gray)
 matplotlib --> RGB image
 """
 
-RGB_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # convert color space from BGR to RGB
-kernel = np.ones((5, 5), np.float32) / 25  # kernel setting
-dst2 = cv.filter2D(RGB_img, -1, kernel)  # output image (opencv is applied)
-dst2_gray = cv.filter2D(gray, -1, kernel)  # output image (opencv is applied)
-
-plt.subplot(121), plt.imshow(RGB_img), plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(dst2), plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.show()
-
-plt.subplot(121), plt.imshow(gray), plt.title('Original Gray')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(dst2_gray), plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
-plt.show()
+# RGB_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # convert color space from BGR to RGB
+# kernel = np.ones((5, 5), np.float32) / 25  # kernel setting
+# dst2 = cv.filter2D(RGB_img, -1, kernel)  # output image (opencv is applied)
+# dst2_gray = cv.filter2D(gray, -1, kernel)  # output image (opencv is applied)
+#
+# plt.subplot(121), plt.imshow(RGB_img), plt.title('Original')
+# plt.xticks([]), plt.yticks([])
+# plt.subplot(122), plt.imshow(dst2), plt.title('Averaging')
+# plt.xticks([]), plt.yticks([])
+# plt.show()
+#
+# plt.subplot(121), plt.imshow(gray), plt.title('Original Gray')
+# plt.xticks([]), plt.yticks([])
+# plt.subplot(122), plt.imshow(dst2_gray), plt.title('Averaging')
+# plt.xticks([]), plt.yticks([])
+# plt.show()
 
 cv.waitKey()
 cv.destroyAllWindows()
